7bb0d379571ab2577095ba396cb072954928a530
duyhau98 <45448179+duyhau98@users.noreply.github.com>
2019-12-03 01:28:28 +0700

    - create login view

---
+++ finalproject/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from "./components/login/login.js";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+
+  <a className="navbar-brand" href="#">FriendShip</a>
+
+  <ul class="navbar-nav">
+    <li className="nav-item">
+    <Link className="nav-link" to="/login">Login</Link>
+    </li>
+
+
+  </ul>
+</nav>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+        <Login />
+          </Route>
+      </Switch>
+
+    </div>
+  </Router>
+  );
+}
+
+export default App;
