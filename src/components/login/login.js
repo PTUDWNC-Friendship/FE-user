3ac5e97ec1962d8fd120ecc74a42af45e2f3cf3c
duyhau98 <45448179+duyhau98@users.noreply.github.com>
2019-12-04 11:56:56 +0700

    fix login onSubmit

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -105,7 +105,7 @@ const styles = theme => ({
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form onSubmit={()=>this.handleSubmit} className={classes.form} noValidate>
+          <form onSubmit={this.handleSubmit} className={classes.form} noValidate>
             <TextField
               variant="outlined"
               margin="normal"
