a9dfbbb598b74504ede565e12da765e2c48d0017
Tom Cao <itom41098@gmail.com>
2019-12-04 11:55:37 +0700

    Seperate homepage header component

---
+++ src/components/home/header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+
+    render() {
+        return (
+          <div className="site-navbar-wrap js-site-navbar bg-white">
+            <div className="container">
+              <div className="site-navbar bg-light">
+                <div className="py-1 bg-white">
+                  <div className="row align-items-center">
+                    <div className="col-2">
+                      <h2 className="mb-0 site-logo"><a href="index.html">Tutor<strong className="font-weight-bold">Finder</strong> </a></h2>
+                    </div>
+                    <div className="col-10">
+                      <nav className="site-navigation text-right" role="navigation">
+                        <div className="container">
+                          <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" className="site-menu-toggle js-menu-toggle text-black"><span className="icon-menu h3"></span></a></div>
+
+                          <ul className="site-menu js-clone-nav d-none d-lg-block">
+                            <li><a href="categories.html">For Candidates</a></li>
+                            <li className="has-children">
+                              <a href="category.html">For Employees</a>
+                              <ul className="dropdown arrow-top">
+                                <li><a href="category.html">Category</a></li>
+                                <li><a href="#">Browse Candidates</a></li>
+                                <li><a href="new-post.html">Post a Job</a></li>
+                                <li><a href="#">Employeer Profile</a></li>
+                                <li className="has-children">
+                                  <a href="#">More Links</a>
+                                  <ul className="dropdown">
+                                    <li><a href="#">Browse Candidates</a></li>
+                                    <li><a href="#">Post a Job</a></li>
+                                    <li><a href="#">Employeer Profile</a></li>
+                                  </ul>
+                                </li>
+
+                              </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="new-post.html"><span className="bg-primary text-white py-3 px-4 rounded"><span className="icon-plus mr-3"></span>Post New Job</span></a></li>
+                          </ul>
+                        </div>
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Header;
--- src/components/home/student.js
+++ src/components/home/student.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Header from './header';
+
 class Student extends React.Component {
 
 
@@ -16,51 +18,7 @@ class Student extends React.Component {
                   <div className="site-mobile-menu-body"></div>
                 </div>
 
-                <div className="site-navbar-wrap js-site-navbar bg-white">
-
-                  <div className="container">
-                    <div className="site-navbar bg-light">
-                      <div className="py-1">
-                        <div className="row align-items-center">
-                          <div className="col-2">
-                            <h2 className="mb-0 site-logo"><a href="index.html">Job<strong className="font-weight-bold">Finder</strong> </a></h2>
-                          </div>
-                          <div className="col-10">
-                            <nav className="site-navigation text-right" role="navigation">
-                              <div className="container">
-                                <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" className="site-menu-toggle js-menu-toggle text-black"><span className="icon-menu h3"></span></a></div>
-
-                                <ul className="site-menu js-clone-nav d-none d-lg-block">
-                                  <li><a href="categories.html">For Candidates</a></li>
-                                  <li className="has-children">
-                                    <a href="category.html">For Employees</a>
-                                    <ul className="dropdown arrow-top">
-                                      <li><a href="category.html">Category</a></li>
-                                      <li><a href="#">Browse Candidates</a></li>
-                                      <li><a href="new-post.html">Post a Job</a></li>
-                                      <li><a href="#">Employeer Profile</a></li>
-                                      <li className="has-children">
-                                        <a href="#">More Links</a>
-                                        <ul className="dropdown">
-                                          <li><a href="#">Browse Candidates</a></li>
-                                          <li><a href="#">Post a Job</a></li>
-                                          <li><a href="#">Employeer Profile</a></li>
-                                        </ul>
-                                      </li>
-
-                                    </ul>
-                                  </li>
-                                  <li><a href="contact.html">Contact</a></li>
-                                  <li><a href="new-post.html"><span className="bg-primary text-white py-3 px-4 rounded"><span className="icon-plus mr-3"></span>Post New Job</span></a></li>
-                                </ul>
-                              </div>
-                            </nav>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <Header/>
 
                 <div style={{height: "113px"}}></div>
 
@@ -68,7 +26,7 @@ class Student extends React.Component {
                   <div className="container">
                     <div className="row align-items-center">
                       <div className="col-12" data-aos="fade">
-                        <h1>Find Job</h1>
+                        <h1>Find Tutor</h1>
                         <form action="#">
                           <div className="row mb-3">
                             <div className="col-md-9">
