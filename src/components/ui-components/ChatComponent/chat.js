bac020397daee50b656f203dbd2e5f6f14804753
duyhau98 <45448179+duyhau98@users.noreply.github.com>
2019-12-16 22:51:08 +0700

    ypadte chat 80% done

--- src/components/ui-components/ChatComponent/chat.js
+++ src/components/ui-components/ChatComponent/chat.js
@@ -3,9 +3,55 @@ import React from 'react';
 import firebase from 'firebase';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import {  Button} from 'react-bootstrap';
+import $ from 'jquery';
 import './chat.css';
 import { fetchAllStudents, fetchAllTutors } from '../../../actions/user';
-import { element } from 'prop-types';
+
+
+
+  function ChatWithPerson(array, user, coWorker) {
+        // eslint-disable-next-line 
+    let allChats = [];
+    
+    array.reverse().forEach( item=> {
+      if(user!==null&&coWorker!==null) {
+      if(item.idSender===user._id&&item.idReceiver===coWorker._id) {
+        const row = <div className="incoming_msg">
+                    <div className="incoming_msg_img"> 
+                          <img src={user!==null?user.imageURL:"https://ptetutorials.com/images/user-profile.png"} alt="sunil" /> 
+                          </div>
+                          <div className="received_msg">
+                          <div className="received_withd_msg">
+                              <p>{item.userName}</p>
+                              <p>{item.message}</p> 
+                              <span className="time_date">{item.time!==null?item.time:''}</span></div>
+                          </div>
+                      </div>;
+          allChats.push(row);
+      }
+    }
+      if(coWorker!==null) {
+        if(item.idSender===coWorker._id && item.idReceiver===user._id) {
+          const row = <div className="outgoing_msg">         
+          <div className="outgoing_msg_img"> 
+           <img src={coWorker!==null?coWorker.imageURL:"https://ptetutorials.com/images/user-profile.png"} alt="sunil" /> 
+           </div>                   
+          <div className="sent_msg">
+            <p>{item.userName}</p>
+            <p>{item.message}</p>
+      <span className="time_date">{item.time!==null?item.time:''}</span> </div>
+        </div> ;
+          allChats.push(row);
+        }
+      }
+
+
+    });
+
+    return allChats;
+}
+
 
 class Chat extends React.Component {
 
@@ -14,6 +60,7 @@ class Chat extends React.Component {
 
           this.state = {
             userName: '',
+            coWorker: null,
             message: '',
             list: [],
             allMessages: [],
@@ -34,21 +81,58 @@ class Chat extends React.Component {
     
     componentDidMount() {
 
-
     }
 
+    getMessageLatestUser(idSender,idUser) {
+      const sortArray = this.state.allMessages;
+      sortArray.sort(function(a,b){
+        // eslint-disable-next-line 
+        return new Date(b.date + " "+b.time) - new Date( a.date + " "+a.time);           
+        });
+ 
+      
+      for(let i = 0 ; i<sortArray.length;i += 1) {
+        // eslint-disable-next-line 
+        if(!sortArray[i].hasOwnProperty('time')) {
+          sortArray.splice(i,1);
+          i -= 1;
+       
+        }
+      }
+      // eslint-disable-next-line 
+      let latestMsg = {message: '', time: ''};
+
+    // Sua lai thanh for thuong
+    for(let i = 0;i<sortArray.length;i+=1) {
+      if(idSender===sortArray[i].idSender&&sortArray[i].idReceiver===idUser) {
+        latestMsg.message = sortArray[i].message;
+        latestMsg.time = sortArray[i].time;
+        break;
+      }
+    }
+      return latestMsg;
+      }
+    
+      setChatWithAnother(coWorker) {
+        this.setState({
+          coWorker
+        });
+      }
 
       handleChange(event) {
         this.setState({message: event.target.value});
       }
       
-      handleSend() {
+
+
+      handleSend(coWorker) {
         if (this.state.message) {
           const { userState } = this.props;
           const {user} = userState;
           if(user!==null) {
             const newItem = {
               idSender: user._id,
+              idReceiver: coWorker._id,
               userName: `${user.firstName  } ${ user.lastName}`,
               message: this.state.message,
               time: (new Date()).toString(),
@@ -60,9 +144,20 @@ class Chat extends React.Component {
         }
       }
 
-      handleKeyPress(event) {
+      handleKeyPress(event, coWorker) {
         if (event.key !== 'Enter') return;
-        this.handleSend();
+        this.handleSend(coWorker);
+      }
+
+      changeCoWorker(coWorker) {
+       
+        if(coWorker!==null) {
+          this.setState({
+            coWorker
+          });
+        
+        }
+
       }
 
     listenMessages() {
@@ -74,8 +169,8 @@ class Chat extends React.Component {
               });
           });
 
+
         this.messageRef
-          .limitToLast(10)
           .on('value', message => {
             this.setState({
                 list: Object.values(message.val()),
@@ -83,41 +178,17 @@ class Chat extends React.Component {
           });
       }
     
-      getMessageLatestUser(idSender) {
-      const sortArray = this.state.allMessages;
-      sortArray.sort(function(a,b){
-        return new Date(b.date + " "+b.time) - new Date(a.date + " "+a.time);           
-        });
-      console.log(sortArray);
       
-      for(let i = 0 ; i<sortArray.length;i++) {
-        if(!sortArray[i].hasOwnProperty('time')) {
-          sortArray.splice(i,1);
-          i--;
-       
-        }
-      }
-      let latestMsg = {message: '', time: ''};
-
-    // Sua lai thanh for thuong
-      sortArray.forEach(element=>{
-
-        if(idSender===element.idSender) {
-          latestMsg.message = element.message;
-          latestMsg.time = element.time;
-         element.stop=true;
-
-        }
-      })
-      return latestMsg;
-      }
-    
     
 
     render() {
+
       const { userState } = this.props;
       const {user, allStudents, allTutors} = userState;
+      let coWorker = null;
+        // eslint-disable-next-line 
       let userName = null;
+         // eslint-disable-next-line 
       let listContacts = [];
       if(user!==null) {
         userName = `${user.firstName  } ${ user.lastName}`;
@@ -126,13 +197,18 @@ class Chat extends React.Component {
           if(user.role==='tutor') {
             
            
-            allStudents.forEach(element => {
-              const latestMsg = this.getMessageLatestUser(element._id);
+            allStudents.forEach((element,index) => {
+              if(index===0) {
+                coWorker = element;
+              }
+              const latestMsg = this.getMessageLatestUser(element._id,user._id);
+              // eslint-disable-next-line 
               let item = {
                 _id: element._id,
+                // eslint-disable-next-line 
                 userName: element.firstName + ' ' + element.lastName,
                 ...latestMsg
-              }
+              };
              
               listContacts.push(item);
 
@@ -141,7 +217,22 @@ class Chat extends React.Component {
         }
         if(allTutors.length>0) {
           if(user.role==='student') {
-          listContacts = user.role==='student'?allTutors:allStudents;
+            allTutors.forEach((element,index) => {
+              if(index===0) {
+                coWorker = element;
+              }
+              const latestMsg = this.getMessageLatestUser(element._id,user._id);
+              // eslint-disable-next-line 
+              let item = {
+              ...element,
+                // eslint-disable-next-line 
+                userName: element.firstName + ' ' + element.lastName,
+                ...latestMsg
+              };
+             
+              listContacts.push(item);
+
+            });
           }
         }
       }
@@ -170,7 +261,7 @@ class Chat extends React.Component {
 
                           {listContacts.map((item,index)=>(
                            index===0?(
-                           <div className="chat_list active_chat">
+                           <Button style={{width: '100%'}} className="chat_list " onClick={()=>this.changeCoWorker(item)}>
                             <div className="chat_people">
                               <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
                               <div className="chat_ib">
@@ -178,9 +269,9 @@ class Chat extends React.Component {
                                   <p>{item.message}</p>
                               </div>
                             </div>
-                          </div>):
+                          </Button>):
                           (
-                            <div className="chat_list">
+                            <Button style={{width: '100%'}} onClick={()=>this.changeCoWorker(item)}  className="chat_list">
                             <div className="chat_people">
                               <div className="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
                               <div className="chat_ib">
@@ -188,7 +279,7 @@ class Chat extends React.Component {
                                 <p>{item.message}</p>
                               </div>
                             </div>
-                          </div>
+                          </Button>
                           )
                           ))}
                      
@@ -196,37 +287,15 @@ class Chat extends React.Component {
                     </div>
                     <div className="mesgs">
                       <div className="msg_history">
-                      { this.state.list.map((item) =>
-                      userName===item.userName ? (<div className="incoming_msg">
-                      <div className="incoming_msg_img"> 
-                      <img src={user!==null?user.imageURL:"https://ptetutorials.com/images/user-profile.png"} alt="sunil" /> 
-                      </div>
-                      <div className="received_msg">
-                      <div className="received_withd_msg">
-                          <p>{item.userName}</p>
-                          <p>{item.message}</p> 
-                          <span className="time_date">{item.time!==null?item.time:''}</span></div>
-                      </div>
-                      </div>) : (
-                          <div className="outgoing_msg">         
-                          <div className="outgoing_msg_img"> 
-                           <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> 
-                           </div>                   
-                          <div className="sent_msg">
-                            <p>{item.userName}</p>
-                            <p>{item.message}</p>
-                      <span className="time_date">{item.time!==null?item.time:''}</span> </div>
-                        </div>)
-                        
-                        )} 
-
+                      
+                      {user!==null&&this.state.coWorker!==null?ChatWithPerson(this.state.allMessages,user,this.state.coWorker):ChatWithPerson(this.state.allMessages,user,coWorker)}
 
 
                       </div>
                       <div className="type_msg">
                         <div className="input_msg_write">
-                          <input type="text" className="write_msg" value={this.state.message}  onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)} placeholder="Type a message" />
-                          <button className="msg_send_btn"  onClick={this.handleSend.bind(this)} type="button"> <i className="fa fa-paper-plane-o" aria-hidden="true" /></button>
+                          <input type="text" className="write_msg" value={this.state.message}  onChange={this.handleChange.bind(this)} onKeyPress={e => this.state.coWorker!==null?this.handleKeyPress(e,this.state.coWorker):this.handleKeyPress(e,coWorker)} placeholder="Type a message" />
+                          <button className="msg_send_btn"  onClick={this.state.coWorker!==null?this.handleSend.bind(this, this.state.coWorker):this.handleSend.bind(this, coWorker)} type="button"> <i className="fa fa-paper-plane-o" aria-hidden="true" /></button>
                         </div>
                       </div>
                     </div>
