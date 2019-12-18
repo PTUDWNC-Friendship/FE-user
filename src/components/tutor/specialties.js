77ffc3e57d730bb9bc52840ef87a0f0512ac105e
Tom Cao <itom41098@gmail.com>
2019-12-18 12:00:26 +0700

    update specialties

--- src/components/tutor/specialties.js
+++ src/components/tutor/specialties.js
@@ -8,18 +8,140 @@ import { fetchUserById } from '../../actions/user';
 class SpecialtyList extends Component {
 
 
+   componentDidMount() {
+     const { user } = this.props.userState;
+     if (user !== null ) {
+       this.props.fetchUserByIdAction(user._id);
+     }
+   }
+
+   showContentTable() {
+     const thTable = ["name", "category", "description", "Actions"];
+     const { tutor } = this.props.userState;
+
+     console.log(tutor);
+     return (
+       <div className="col-md-12" data-aos="fade">
+         <Grid fluid>
+          <div className="site-section bg-light">
+            <div className="container">
+              <div className="table-wrapper">
+                <div className="table-title">
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <h2>
+                        Your <b>Specialties</b>
+                      </h2>
+                    </div>
+                    <div className="col-sm-6">
+                      <a
+                        href="#addSpecialtyModal"
+                        className="btn btn-success"
+                        data-toggle="modal"
+                      >
+                        <i className="material-icons">&#xE147;</i>{' '}
+                        <span>Add New Specialty</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      {thTable.map((value, index) => {
+                        return <th key={index.toString()}>{value}</th>;
+                      })}
+                    </tr>
+                  </thead>
+                  <tbody>
+                     {tutor !== null ? tutor.subjects.map((value, index) => {
+                       return (
+                         <tr key={index.toString()}>
+                           <td>{value.name}</td>
+                           <td>{value.category}</td>
+                           <td>{value.description}</td>
+                           <td>
+                             <a
+                               href="#editSpecialtyModal"
+                              className="edit"
+                              data-toggle="modal"
+                            >
+                              <i
+                                className="material-icons"
+                                data-toggle="tooltip"
+                                title="Edit"
+                              >
+                                &#xE254;
+                              </i>
+                            </a>
+                            <a
+                              href="#deleteSpecialtyModal"
+                              className="delete"
+                              data-toggle="modal"
+                            >
+                              <i
+                                className="material-icons"
+                                data-toggle="tooltip"
+                                title="Delete"
+                              >
+                                &#xE872;
+                              </i>
+                            </a>
+                          </td>
+                        </tr>
+                      );
+                    }) : null}
+                  </tbody>
+                </table>
+                <div className="clearfix">
+                  <ul className="pagination">
+                    <li className="page-item disabled">
+                      <a href="#">Previous</a>
+                    </li>
+                    <li className="page-item">
+                      <a href="#" className="page-link">
+                        1
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a href="#" className="page-link">
+                        2
+                      </a>
+                    </li>
+                    <li className="page-item active">
+                      <a href="#" className="page-link">
+                        3
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a href="#" className="page-link">
+                        4
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a href="#" className="page-link">
+                        5
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a href="#" className="page-link">
+                        Next
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Grid>
+      </div>
+    );
+  };
+
   render() {
-
-    const subject = [];
-    subject.push(<h>Default</h>);
-
-    const { userState } = this.props;
-    console.log(userState.user);
-
     return (
       <div>
         <div style={{ height: '113px' }} />
-
         <div
           className="unit-5 overlay"
           style={{
@@ -42,13 +164,14 @@ class SpecialtyList extends Component {
                       <div className="modal-body">
                         <div className="form-group">
                           <h1>Subject</h1>
-                          <input type="text" className="form-control" required/>
-                        </div>
-                        <div className="form-group">
-                          <h1>Category</h1>
-                          <select
-                            className="form-control"
-                          >
+                           <input type="text" className="form-control" required/>
+                         </div>
+                         <div className="form-group">
+                           <h>Category</h>
+                           <h1>Category</h1>
+                           <select
+                             className="form-control"
+                           >
                             <option value="math">Math</option>
                             <option value="literature">Literature</option>
                             <option value="biology">Biology</option>
@@ -57,13 +180,14 @@ class SpecialtyList extends Component {
                             <option value="physics">Physics</option>
                             <option value="chemistry">Chemistry</option>
                             <option value="history">History</option>
-                          </select>
-                        </div>
-                        <div className="form-group">
-                          <h1>Description</h1>
-                          <textarea className="form-control" required />
-                        </div>
-                      </div>
+                           </select>
+                         </div>
+                         <div className="form-group">
+                           <h>Description</h>
+                           <h1>Description</h1>
+                           <textarea className="form-control" required />
+                         </div>
+                       </div>
                       <div className="modal-footer">
                         <input type="button" className="btn btn-default" data-dismiss="modal" value="Cancel"/>
                         <input type="submit" className="btn btn-success" value="Add"/>
@@ -80,17 +204,19 @@ class SpecialtyList extends Component {
                       <div className="modal-header">
                         <h4 className="modal-title">Edit Specialty</h4>
                         <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                      </div>
-                      <div className="modal-body">
-                        <div className="form-group">
-                          <h1>Subject</h1>
-                          <input type="text" className="form-control" required/>
-                        </div>
-                        <div className="form-group">
-                          <h1>Category</h1>
-                          <select
-                            className="form-control"
-                          >
+                       </div>
+                       <div className="modal-body">
+                         <div className="form-group">
+                           <h>Subject</h>
+                           <h1>Subject</h1>
+                           <input type="text" className="form-control" required/>
+                         </div>
+                         <div className="form-group">
+                           <h>Category</h>
+                           <h1>Category</h1>
+                           <select
+                             className="form-control"
+                           >
                             <option value="math">Math</option>
                             <option value="literature">Literature</option>
                             <option value="biology">Biology</option>
@@ -136,28 +262,25 @@ class SpecialtyList extends Component {
                 </div>
               </div>
 
+
               { this.showContentTable() }
             </div>
           </div>
         </div>
-
       </div>
     );
   }
 }
-
-const mapStateToProps = state => {
+const mapStateTopProps = state => {
   return {
     contractState: state.contractState,
     userState: state.userState
   };
 };
-
 const mapDispatchToProps = dispatch => {
   return {
     fetchAllContractsAction: () => dispatch(fetchAllContracts()),
     fetchUserByIdAction: id => dispatch(fetchUserById(id))
   };
 };
-
-export default connect(mapStateToProps, mapDispatchToProps)(SpecialtyList);
+export default connect(mapStateTopProps, mapDispatchToProps)(SpecialtyList);
