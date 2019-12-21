5d31dae02bf1d145f449486fe73fb123b4dca9f5
Dinh Hai <dinhhhaii@gmail.com>
2019-12-21 22:20:40 +0700

    update verification

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,7 +27,7 @@ import Logout from '../components/logout/logout';
 import Chat from '../components/ui-components/ChatComponent/chat';
 import Contract from '../components/contract/contract';
 import SearchContainer from '../search/container';
-import Verification from '../components/verification/index';
+import Verification from '../components/account/verification';
 
 const routes = [
   {
